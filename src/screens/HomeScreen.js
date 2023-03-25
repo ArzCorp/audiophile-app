b3b9f39b147dba39a974ecc82de2e6558c83865a
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-03-24 20:13:15 -0600

    🐛 fix env import

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import IconButton from '../components/IconButton';
+import { API_URL } from '@env';
 
 export default function HomeScreen() {
   console.log(API_URL);
