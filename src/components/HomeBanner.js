a8904baa505c940b34b069fb2d8b2eaf5a288e7f
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-05-27 09:52:48 -0600

    🎨 improve home banner styles

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import Button from './Button';
-import HomeBannerImage from '../assets/home-banner.png';
+import HomeBannerImage from '../assets/images/home-banner.png';
 import { COLORS } from '../utils/constans';
+import TextField from './TextField.js';
 
 export default function HomeBanner() {
   return (
-    <View style={styles.banner__container}>
-      <Image style={styles.banner__image} source={HomeBannerImage} />
-      <Text style={[styles.banner__text, styles['banner__text--new']]}>
-        NEW PRODUCT
-      </Text>
-      <Text style={[styles.banner__text, styles['banner__text--title']]}>
-        XX99 Mark II HeadphoneS
-      </Text>
-      <Text style={[styles.banner__text, styles['banner__text--details']]}>
-        Experience natural, lifelike audio and exceptional build quality made
-        for the passionate music enthusiast.
-      </Text>
-      <View style={styles['banner__button--container']}>
-        <Button title="SEE PRODUCT" />
+    <View>
+      <View style={styles.banner__container}>
+        <Image style={styles.banner__image} source={HomeBannerImage} />
+        <TextField
+          size="overline"
+          align="center"
+          style={styles['banner__text--new']}>
+          NEW PRODUCT
+        </TextField>
+        <TextField
+          size="h2"
+          align="center"
+          weight="bold"
+          uppercase
+          style={styles['banner__text--title']}>
+          {`XX99 Mark II\nHeadphones`}
+        </TextField>
+        <TextField
+          weight="medium"
+          align="center"
+          style={styles['banner__text--details']}>
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </TextField>
+        <View style={styles['banner__button--container']}>
+          <Button title="SEE PRODUCT" />
+        </View>
       </View>
     </View>
   );
@@ -27,34 +41,28 @@ export default function HomeBanner() {
 
 const styles = StyleSheet.create({
   banner__container: {
+    position: 'relative',
+    zIndex: 0,
     paddingTop: 108,
     paddingBottom: 116,
     backgroundColor: COLORS.BLACK,
   },
-  banner__text: {
-    color: COLORS.WHITE,
-    textAlign: 'center',
-  },
   ['banner__text--new']: {
     opacity: 0.5,
     marginBottom: 24,
-    fontSize: 14,
   },
   ['banner__text--title']: {
-    fontSize: 36,
-    fontWeight: 'bold',
     marginBottom: 24,
   },
   ['banner__text--details']: {
     opacity: 0.75,
-    fontSize: 15,
-    paddingHorizontal: 24,
+    paddingHorizontal: 48,
     marginBottom: 28,
   },
   banner__image: {
     width: '100%',
     position: 'absolute',
-    top: -100,
+    top: -90,
     left: 0,
     right: 0,
     bottom: 0,
