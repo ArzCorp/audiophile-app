cb94d89f7b0b37c82f02b9b16e8049e0105ca918
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-06-06 08:08:21 -0600

    🔥 remove usless screens

--- src/screens/EarphonesScreen.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-export default function EarphonesScreen() {
-  return <Text>Earphones Screen</Text>;
-}
--- src/screens/HeadphonesScreen.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-export default function HeadphonesScreen() {
-  return <Text>Headphones Screen</Text>;
-}
